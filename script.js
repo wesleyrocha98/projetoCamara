b442aaaf3976e4cfec8817840b5cf10387d315d7
pablojr17 <pablojr17@gmail.com>
2024-12-07 16:19:07 -0300

    Ajustes gerais

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let currentIndex = 0;
 const items = document.querySelectorAll('.carousel-item');
 const totalItems = items.length;
 
+function updateActiveClass() {
+  items.forEach((item, index) => {
+    item.classList.toggle('active', index === currentIndex);
+  });
+}
+
 function showSlide(index) {
   const carousel = document.querySelector('.carousel');
+  if (!carousel) return; // Verifica se o elemento existe
   const offset = -index * 100; // Move 100% para cada slide
   carousel.style.transform = `translateX(${offset}%)`;
+  updateActiveClass();
 }
 
 function nextSlide() {
@@ -17,6 +25,12 @@ function prevSlide() {
   currentIndex = (currentIndex - 1 + totalItems) % totalItems; // Volta ao final ao alcançar o início
   showSlide(currentIndex);
 }
+
+function toggleMenu() {
+  const menu = document.querySelector('.nav-header-container');
+  menu.classList.toggle('open');
+}
+
 
 // Alterna slides automaticamente a cada 10 segundos
 setInterval(nextSlide, 10000);
